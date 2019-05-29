e8b01620c318958829b2a05b33a2e045b7370a43
beschwa <naraiswar@gmail.com>
2019-05-29 10:12:36 -0400

    5/28 EOD

--- src/component/GameCard.js
+++ src/component/GameCard.js
@@ -11,6 +11,7 @@ function GameCard(props) {
 			}
 	}
 	return (
+		
 		<div className="card" onClick={()=>{props.onGameClick(props.game)}}>
 			<div className="image">
 				<img className="equal" src={getImage()} />
